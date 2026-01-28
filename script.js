2c40071004ec48ed0abd17b2db01402a70429a58
Admin Bisa Tour <admin@bisatour.com>
2026-01-28 23:35:06 +0700

    update index

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// Toggle Menu Navbar untuk Mobile
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 
+// Buka Tutup Menu
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// Tutup menu kalau salah satu link diklik
+// TUTUP MENU OTOMATIS SAAT LINK DIKLIK (PENTING BUAT HP)
 document.querySelectorAll('.nav-links li a').forEach(link => {
     link.addEventListener('click', () => {
         navLinks.classList.remove('active');
     });
+});
+// HILANGKAN PRELOADER SAAT WEB SELESAI DIMUAT
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    setTimeout(() => {
+        preloader.classList.add('loaded');
+    }, 2000); // Tahan 2 detik biar Bismillah terbaca
 });
